ce3a5f09aeb15c4d61cde9f22e9b76001da413e6
Kurtis Best <dacobra3@hotmail.com>
2018-10-28 19:37:33 -0400

    feat: update library

--- lib/persistence/data/database-object.d.ts
+++ lib/persistence/data/database-object.d.ts
@@ -1,22 +1,42 @@
 import { IDbInfo } from './data-objects';
 import * as PouchDB from 'pouchdb';
+/**
+ * Creates a new DatabaseServerConnection object
+ */
+export declare class DatabaseServerConnection {
+    readonly server: string;
+    readonly databaseName: string;
+    readonly user: string;
+    readonly password: string;
+    readonly timeout: number;
+    readonly secure: boolean;
+    /**
+     * Creates a new DatabaseServerConnection
+     * @param server Host or location of the server
+     * @param databaseName Name of the database trying to connection to
+     * @param user Username to connect to the database
+     * @param password Password associated with the database
+     * @param timeout Time out period
+     * @param secure Indicates whether the connection should be secured. Defaults to false
+     */
+    constructor(server: string, databaseName: string, user: string, password: string, timeout: number, secure?: boolean);
+    /**
+     * Creates a new DatabaseServerConnection with an existing connection and a new database
+     * @param oldConnection existing DatabaseServerConnection
+     * @param newDatabase New database to be applied
+     */
+    static createNewConnection(oldConnection: DatabaseServerConnection, newDatabase: string): DatabaseServerConnection;
+}
 export declare class DatabaseObject {
     private initialized;
     private _db;
-    constructor(connection: string | {
-        server: string;
-        databaseName: string;
-        user: string;
-        password: string;
-        timeout: number;
-    });
+    constructor(connection: string | DatabaseServerConnection);
     /**Gets whether the database has been initialised */
     readonly isInitialized: boolean;
     /**Gets the database used */
     readonly database: PouchDB;
     status: string;
     /**
-     *
      * Establishes the connection for remote host
      * @param connection Connection information required to connect to the database
      * @param databaseName Name of the database
---
+++ lib/persistence/data/lucene-data-objects.d.ts
@@ -0,0 +1,54 @@
+import { Entity, IEntityMapBuilder } from 'caribviper-entity';
+/**Implementation for Lucene searches against the db */
+export declare class LuceneFetchOptions {
+    luceneServer: string;
+    databaseName: string;
+    indexPath: string;
+    q: string;
+    include_docs: boolean;
+    limit: number;
+    skip: number;
+    /**
+     * Creates a lucene fetch option
+     * @param luceneServer Location to the lucene server
+     * @param databaseName Name of the database to execute fetch against
+     * @param indexPath Path to the index e.g. lucene\by_name
+     * @param q Query parameter being sought
+     * @param include_docs Indicates whether to include documents
+     * @param limit Specify the default limit of records to return, default is 25
+     * @param skip Number of th records to skip
+     */
+    constructor(luceneServer: string, databaseName: string, indexPath: string, q: string, include_docs?: boolean, limit?: number, skip?: number);
+    readonly url: string;
+}
+/**Implements a lucene scored row result */
+export declare class LuceneScoredRow {
+    score: number;
+    doc: any;
+    id: string;
+    /**
+     * Converts a document to the desired entity type
+     * @param mapBuilder Entity type to convert to
+     */
+    convertDocument<T extends Entity>(mapBuilder: IEntityMapBuilder<T>): T;
+    /**
+     * Clone a row to a new LuceneScoredRow
+     * @param row Row to be cloned
+     */
+    static clone(row: LuceneScoredRow): LuceneScoredRow;
+    /**
+     * Assigns a  row to itself as new LuceneScoredRow
+     * @param row
+     */
+    static reAssign(row: LuceneScoredRow): void;
+}
+/**Fetch results from a lucene search */
+export declare class LuceneFetchResults {
+    q: string;
+    fetch_duration: number;
+    total_rows: number;
+    limit: number;
+    search_duration: number;
+    skip: number;
+    rows: LuceneScoredRow[];
+}
